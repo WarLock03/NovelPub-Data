26add608843fc01299075225c57dcd2c4702affa
WarLock03 <150971007+WarLock03@users.noreply.github.com>
2023-11-19 18:09:13 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let itemTable = document.getElementById('table-item');
 let tableRow = itemTable.querySelectorAll('tr');
-
-for(let i in tableRow){
-  tableRow[i].addEventListener('click', function(){
-    tableRow[i].setAttribute("data-bs-toggle","modal");
-    tableRow[i].setAttribute("data-bs-target","#myModal");
+tableRow.forEach((row) => {
+  row.addEventListener("click", function(){
+    row.setAttribute("data-bs-toggle","modal");
+    row.setAttribute("data-bs-target","#myModal");
   });
-}
+});
