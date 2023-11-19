93ddf2066aa00d12082efc45fa06d26c09c26ba1
WarLock03 <150971007+WarLock03@users.noreply.github.com>
2023-11-19 19:56:28 +0800

    update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function showAllData(){
   let table2 = document.getElementById('table-item2');
   let showAll = document.getElementById('showAll');
   
-  if(table2.style.display == "none"){
+  if(table.style.display == "block"){
     table.style.display = "none";
     table2.style.display = "block";
     showAll.innerText = "‹‹ Show Less ››";
