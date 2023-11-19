1f1fb6d07fffe016eebdc9accc9e4414521d6a2b
WarLock03 <150971007+WarLock03@users.noreply.github.com>
2023-11-19 18:42:19 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function showAllData(){
+/*function showAllData(){
   let table = document.getElementById('table-item');
   let table2 = document.getElementById('table-item2');
   let showAll = document.getElementById('showAll');
@@ -13,4 +13,15 @@ function showAllData(){
     showAll.innerText = "‹‹ Show All ››";
   }
   
-}
+}*/
+$(document).ready(function(){
+  let rowLimit = 10;
+  $('table > #tbody > tr:gt(' + (rowLimit - 1) + ')');
+  
+  let table = $('#table-item');
+  
+  $('#showAll').click(function(){
+    table.add
+  });
+  
+});
